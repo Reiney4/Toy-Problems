50486e23c7ec7a6170e875d689bd1b371f80560a
Valerie <valeriejeptoo4@gmail.com>
2023-08-20 00:26:30 +0300

    used conditional statements to convert time

--- challenges/conversion.py
+++ challenges/conversion.py
@@ -1,9 +1,12 @@
-#created a function convert_24hour
+#created a function convert_24hour with three arguments
 
-def convert_24hour(hour, minute, period):
+def convert_to_24_hour(hour, minute, period):
     if period == "pm":
         hour = (hour + 12) % 24 if hour != 12 else 12
     else:
         hour = hour % 12
 
     return f"{hour:02d}{minute:02d}"
+
+# Directly use the function and print the result
+print(convert_to_24_hour(8, 30, "am"))
