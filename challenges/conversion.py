83d9bcfe5a83e83544ca998f264c8ba7e5174e0c
Valerie <valeriejeptoo4@gmail.com>
2023-08-20 00:08:01 +0300

    used loop to check for positive numbers

--- challenges/conversion.py
+++ challenges/conversion.py
@@ -1,2 +1,7 @@
-#convert time to 24 -hour time
-def time_conversion
+def convert_to_24_hour(hour, minute, period):
+    if period == "pm":
+        hour = (hour + 12) % 24 if hour != 12 else 12
+    else:
+        hour = hour % 12
+
+    return f"{hour:02d}{minute:02d}"
--- challenges/positive_numbers.py
+++ challenges/positive_numbers.py
@@ -1,4 +1,19 @@
-#create a function with three intergers as the arguments
+   #create a function positive_count with three arguments
 
-def positive_numbers(a, b, c):
-    return [True if  num > 0 else False for num in num_list]
+def positive_count(a, b, c):
+   
+   
+    # The function returns True if at least two of the arguments are positive
+    
+    positive_nums = [num for num in (a, b, c) if num > 0]
+    
+    if len(positive_nums) >= 2:
+        return True
+    #otherwise returns false
+    else:
+        return False
+
+
+result = positive_count(10, -5, 2)
+print(result)  
+
